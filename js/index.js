e1d88dfe7917ba8e596dc9f8f1221eb16aa4afda
Enrique <aecaridad@alumnos.cei.es>
2026-06-15 21:29:24 +0200

    feat: enhance hero section with new card layout and image carousel functionality

--- js/index.js
+++ js/index.js
@@ -1,24 +1,21 @@
-
-
 // Hero Section
-// Aumentar el indice del carrrusel mini
-// Realizar todo en una funcion sincronizada
-const arrayImg = ['media/img/Ciberpunk__hero.jpg', 'media/img/Rs__hero.jpg', 'media/img/Ciberpunk__hero.jpg']
+
+const heroRotativo = document.getElementById('hero-rotativo')
+const imagenRotativa = document.getElementById('imagen-rotativa')
+const imageContainter = document.getElementById('image-container')
+const heroRotativoSrcset = document.getElementById('hero-rotativo-srcset')
+const arrayImg = ['media/img/ciberpunk.jpg', 'media/img/resident.jpg']
 let indiceArray = 0
 let interval
 const delayImg = 3000
 
-const imagenRotativa = document.getElementById('imagen-rotativa')
-
 function cambioImg() {
     
-    imagenRotativa.style.opacity = 0
     setTimeout( () => {
         indiceArray++
         indiceArray >= arrayImg.length ? indiceArray = 0 : ""
-        // Es otra forma de escribir el if y el else
-        imagenRotativa.src = arrayImg[indiceArray]
-        imagenRotativa.style.opacity = '100%'
+        heroRotativo.src = arrayImg[indiceArray]
+        heroRotativoSrcset.srcset = arrayImg[indiceArray]
     }, 300)
 }
     
@@ -30,8 +27,9 @@ function detenerIntervalo() {
     clearInterval(interval)
 }
 
-imagenRotativa.addEventListener('mouseenter', detenerIntervalo)
-imagenRotativa.addEventListener('mouseleave', iniciarIntervalo)
+// imagenRotativa.addEventListener('mouseenter', detenerIntervalo)
+// imagenRotativa.addEventListener('mouseleave', iniciarIntervalo)
 
 iniciarIntervalo()
+
 //  / Hero Carrusel
