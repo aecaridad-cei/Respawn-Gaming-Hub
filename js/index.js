9b83fcddbeb5f0ef1a564775fcbbbf8add7249b0
Enrique <aecaridad@alumnos.cei.es>
2026-06-15 19:23:33 +0200

    feat: prototype of the hero section carousel

---
+++ js/index.js
@@ -0,0 +1,35 @@
+
+
+// Hero Carrusel
+const arrayImg = ['media/img/Ciberpunk__hero.jpg', 'media/img/rs__hero.jpg', 'media/img/Ciberpunk__hero.jpg']
+let indiceArray = 0
+let interval
+const delayImg = 3000
+
+const imagenRotativa = document.getElementById('imagen-rotativa')
+
+function cambioImg() {
+    
+    imagenRotativa.style.opacity = 0
+    setTimeout( () => {
+        indiceArray++
+        indiceArray >= arrayImg.length ? indiceArray = 0 : ""
+        // Es otra forma de escribir el if y el else
+        imagenRotativa.src = arrayImg[indiceArray]
+        imagenRotativa.style.opacity = '100%'
+    }, 300)
+}
+    
+function iniciarIntervalo() {
+    interval = setInterval(cambioImg, delayImg)
+}
+
+function detenerIntervalo() {
+    clearInterval(interval)
+}
+
+imagenRotativa.addEventListener('mouseenter', detenerIntervalo)
+imagenRotativa.addEventListener('mouseleave', iniciarIntervalo)
+
+iniciarIntervalo()
+//  / Hero Carrusel
