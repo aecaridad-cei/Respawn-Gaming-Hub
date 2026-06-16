48e533116f96d4432999f480be62be5561052a98
Enrique <aecaridad@alumnos.cei.es>
2026-06-16 19:11:19 +0200

    feat: update hero section images and enhance image carousel functionality

--- js/index.js
+++ js/index.js
@@ -1,22 +1,25 @@
 // Hero Section
 
 const heroRotativo = document.getElementById('hero-rotativo')
-const imagenRotativa = document.getElementById('imagen-rotativa')
-const imageContainter = document.getElementById('image-container')
+let imagenRotativaA = document.getElementById('imagen-rotativa-a')
+let imagenRotativaB = document.getElementById('imagen-rotativa-b')
+let imagenRotativaC = document.getElementById('imagen-rotativa-c')
+let imageContainter = document.getElementById('image-container')
 const heroRotativoSrcset = document.getElementById('hero-rotativo-srcset')
-const arrayImg = ['media/img/ciberpunk.jpg', 'media/img/resident.jpg']
+const arrayImg = ['media/img/img_1.jpg', 'media/img/img_2.jpg','media/img/img_3.jpg', 'media/img/img_4.jpg']
 let indiceArray = 0
 let interval
 const delayImg = 3000
 
 function cambioImg() {
-    
     setTimeout( () => {
         indiceArray++
         indiceArray >= arrayImg.length ? indiceArray = 0 : ""
         heroRotativo.src = arrayImg[indiceArray]
         heroRotativoSrcset.srcset = arrayImg[indiceArray]
+        
     }, 300)
+
 }
     
 function iniciarIntervalo() {
